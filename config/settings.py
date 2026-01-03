188cdc52e5c59ff8c83fc0c0ed6c469f4b462ff9
Zafar Abdul <zafarabdul05@gmail.com>
2026-01-03 19:48:50 +0530

    config: Disable debug mode and allow all hosts.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8^e&lo!k*@5#awop3u93!t%4dv9qd=+q#z*(jygizkpq%#ed%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
